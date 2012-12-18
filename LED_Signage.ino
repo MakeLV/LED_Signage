6583b658d9753362125b173ac589ef7600fcf22d
Josiah Ritchie <josiah@josiahritchie.com>
2012-12-18 15:36:03 -0500

    Getting the rows working, the electornics still needs to catch up (need to desolder the transistor array from the old controller)

--- LED_Signage.ino
+++ LED_Signage.ino
@@ -2,29 +2,46 @@ int clk = 11;
 int ser = 12;
 byte count = 0;
 
+// Array of pins the rows coorespond to
+int rows[] = { 8, 9, 10, 13, 7, 6, 5 };
+int rowCount = 7;
+
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(clk, OUTPUT);     
   pinMode(ser, OUTPUT);
+
+  for (int row = 0; row < rowCount; row++)  {
+    pinMode(rows[row], OUTPUT);
+  }
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
 
+  // Prepare to set the next bit
   digitalWrite(clk, LOW);
   
-    if (count%4 == 0) 
+    // Decide if bit should be on or off
+    if (count%2 == 0) 
     { 
       digitalWrite(ser, LOW);
-    //  count = 1;
     } else {
       digitalWrite(ser, HIGH);
-    //  count = 0;
     }
-  
-  delay(1);  
+
+  // Push the bit onto the shift register
   digitalWrite(clk, HIGH);
+
+
+  // Iterate through the rows, turning one on at a time
+  for (int row = 0; row < rowCount; row++) {
+    digitalWrite(rows[row], HIGH);
+    delay(100);
+    digitalWrite(rows[row], LOW);
+  }
+
   count++;
-  delay(200);
+  delay(100);
 }
