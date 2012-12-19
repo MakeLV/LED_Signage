64e2910ce080f0b7c025f4f17ab4c193f8a705f9
Josiah Ritchie <josiah@josiahritchie.com>
2012-12-19 04:14:10 -0500

    finalizing the row testing

--- LED_Signage.ino
+++ LED_Signage.ino
@@ -1,24 +1,24 @@
-int clk = 2;
-int ser = 3;
-byte count = 0;
+int clk = 2; // Clock
+int ser = 3; // Serial
 
 // Array of pins the rows coorespond to
 int rows[] = { 8, 9, 10, 11, 7, 6, 5 };
-int rowCount = 7;
+int rowCount = 5; //Actually 7, but only 5 wired up atm
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
+  // initialize the serial and clock pins as outputs.
   pinMode(clk, OUTPUT);     
   pinMode(ser, OUTPUT);
 
-  // setup the row pins
+  // setup the row pins, make sure they're off
   for (int row = 0; row < rowCount; row++)  {
     pinMode(rows[row], OUTPUT);
-    digitalWrite(rows[row], LOW);
+    digitalWrite(rows[row], HIGH);
   }
 
-  // Shift 75 0s onto the register to clear it  
+  // Shift 75 0s onto the register to clear it, doing 
+  // it this way because even in decimal, that's a big number
   for (int clear = 0; clear <= 15; clear++) {
     shiftOut(ser, clk, MSBFIRST, 00000);
   }
@@ -28,16 +28,17 @@ void setup() {
 void loop() {
 
   // Count to 256, but put a buffer of 4 bits in between
-  for (int bits = 0;  bits < 1024;  bits++) {
+  for (int bits = 0;  bits < 256;  bits++) {
     shiftOut(ser, clk, MSBFIRST, bits);
     shiftOut(ser, clk, MSBFIRST, B0000);
-    if (count < 5) {
-      digitalWrite(rows[count], LOW);
-      digitalWrite(rows[count-1], HIGH);
-      count++;
-    } else {
-      count = 0;
+
+    // show this on each row once before moving on
+    for (int row = 0; row < rowCount; row++) {
+      digitalWrite(rows[row], LOW);
+      delay(100);
+      digitalWrite(rows[row], HIGH);
     }
+
     delay(1000);
   }
 }
