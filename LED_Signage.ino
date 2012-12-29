d2c69492149ba8845da013e32db4cf55ad3bd3cd
Josiah Ritchie <josiah@josiahritchie.com>
2012-12-29 16:51:40 -0500

    resolving conflict

--- LED_Signage.ino
+++ LED_Signage.ino
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 int clk = 2;
 int ser = 3;
 byte count = 0;
@@ -5,43 +6,59 @@ byte count = 0;
 // Array of pins the rows coorespond to
 int rows[] = { 8, 9, 10, 11, 7, 6, 5 };
 int rowCount = 5;
+=======
+int clk = 2; // Clock
+int ser = 3; // Serial
+
+// Array of pins the rows coorespond to
+int rows[] = { 8, 9, 10, 11, 7, 6, 5 };
+int rowCount = 5; //Actually 7, but only 5 wired up atm
+>>>>>>> ShiftOut
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
+  // initialize the serial and clock pins as outputs.
   pinMode(clk, OUTPUT);     
   pinMode(ser, OUTPUT);
 
+  // setup the row pins, make sure they're off
   for (int row = 0; row < rowCount; row++)  {
     pinMode(rows[row], OUTPUT);
+    digitalWrite(rows[row], HIGH);
+  }
+
+  // Shift 75 0s onto the register to clear it, doing 
+  // it this way because even in decimal, that's a big number
+  for (int clear = 0; clear <= 15; clear++) {
+    shiftOut(ser, clk, MSBFIRST, 00000);
   }
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
 
-  // Prepare to set the next bit
-  digitalWrite(clk, LOW);
-  
-    // Decide if bit should be on or off
-    if (count%2 == 0) 
-    { 
-      digitalWrite(ser, LOW);
-    } else {
-      digitalWrite(ser, HIGH);
+  // Count to some number, but put a buffer of blank bits in between
+  for (int bits = 0;  bits < 2048; bits++) {
+    shiftOut(ser, clk, MSBFIRST, bits);
+    shiftOut(ser, clk, MSBFIRST, B00000000);
+
+    // show this on each row once before moving on
+    for (int row = 0; row < rowCount; row++) {
+      digitalWrite(rows[row], LOW);
+      delay(100);
+      digitalWrite(rows[row], HIGH);
     }
 
-  // Push the bit onto the shift register
-  digitalWrite(clk, HIGH);
-
-
+<<<<<<< HEAD
   // Iterate through the rows, turning one on at a time
   for (int row = 0; row < rowCount; row++) {
     digitalWrite(rows[row], LOW);
     delay(100);
     digitalWrite(rows[row], HIGH);
+=======
+    digitalWrite(rows[0], LOW);
+    delay(1000);
+    digitalWrite(rows[0], HIGH);
+>>>>>>> ShiftOut
   }
-
-  count++;
-  delay(100);
 }
