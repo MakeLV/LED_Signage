a72ec5ea302c28aabb6b9a513994449ececc0e80
Josiah Ritchie <josiah@josiahritchie.com>
2013-01-01 00:55:12 -0500

    Bringing in some code from the GPL3'd ht1632c Arduino Library found on code.google.com

--- LED_Signage.ino
+++ LED_Signage.ino
@@ -1,3 +1,5 @@
+#include "font.h"
+
 int clk = 2; // Clock
 int ser = 3; // Serial
 
@@ -45,4 +47,46 @@ void loop() {
     delay(100);
     digitalWrite(rows[row], HIGH);
   }
+}
+
+/* lights up the display so you can see the message */
+
+void light_up_display() {
+  for (int row = 0; row < rowCount; row++) {
+    digitalWrite(rows[row], LOW);
+    delay(2);
+    digitalWrite(rows[row], HIGH);
+  }
+}
+
+/* print a single character 
+ * from: http://code.google.com/p/ht1632c/source/browse/ht1632c.cpp?r=f32a1046dbffe7417cc068d186b0bd3c17a28271
+*/
+
+byte putchar (int x, int y, char c, byte color, byte attr)
+{
+  byte dots, width = 5;
+
+  Serial.println(c, DEC);
+  c -= 32;
+
+  for (char col=0; col < width; col++) {
+    // some math with pointers... don't try this at home ;-)
+    prog_char *addr = font + c * width + col;
+    dots = pgm_read_byte_near(addr);
+
+    if (dots) {
+      for (char row = 0; row < font_height; row++) {
+        if (dots & (128>>row))
+          plot(x+col, y+row, color);
+        else 
+          plot(x+col, y+row, BLACK);
+
+      }
+    } else if (attr & PROPORTIONAL) {
+      width--;
+      x--;
+    }
+  }
+  return width;
 }
